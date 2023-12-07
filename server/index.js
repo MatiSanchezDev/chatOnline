1baca85f5790bc25c27352ce331df58f15c83149
MatiSanchezDev <sanchezmatidev@gmail.com>
2023-12-07 13:57:32 -0300

    Servidor http y socked funcionando

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import logger from "morgan";
+
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
+const port = process.env.PORT ?? 1234;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user has connected!");
+
+  socket.on("disconnect", () => {
+    console.log("an user hast disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
+});
+
+server.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto: http://localhost:${port}`);
+});
